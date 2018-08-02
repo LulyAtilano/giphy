11b20fe4ab042349664bfee70c3b540fce57ef56
latilanoh@gmail.com <latilanoh@gmail.com>
2018-08-02 01:10:45 -0500

    se realizaron completo la funcionalidad en app.js y se cambio el cdn de jquery

--- js/app.js
+++ js/app.js
@@ -10,29 +10,33 @@ $(document).ready(function() {
     }
 
     var armarTemplate = function(gif,url) {
-        var t = "div class='elemento'>
-                    <img src='" + gif + "'/>
-                        <a href='" + url + "'> Ver más </a>
-                </div>"
+        var t= "div class='elemento'><img src='" + gif + "'/><a href='" + url + "'> See more </a></div>"
         return t;
     }
-});
 
-var ajaxGif = function(gif) {
-    $.ajax ({
-        url: 'http:api.giphy.com/v1/gifs/search',
-        type: 'GET',
-        datatype: 'json',
-        data: {
-            q: gif,
-            api_key: 'dc6zaTOxFJmzC'
-        }
-    })
-    .done(function(response) {
-        console.log(response);
-        dibujarGifs(response.data);
-    })
-    .fail(function() {
-        console.log("error");
+    var ajaxGif = function(gif) {
+        $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?',
+            type: 'GET',
+            datatype: 'json',
+            data: {
+                q: gif,
+                api_key: 'P84KIqeOxMX4BkzM8nbh97dj38fVYOW8'
+            }
+        })
+        .done(function(response) {
+            console.log(response);
+            dibujarGifs(response.data);
+        })
+        .fail(function() {
+            console.log("error");
+        });
+    }
+
+    $("#buscar-gif").click(function(event) {
+        console.log("Entro");
+        $("#elementos").empty();
+        var gif= $("#gif-text").val();
+        ajaxGif(gif);
     });
-}
+});
